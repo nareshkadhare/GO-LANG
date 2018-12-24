d8689a6e79ae08f9153d2c52bc6897bb916e99f2
nareshkadhare <35559390+nareshkadhare@users.noreply.github.com>
2018-12-24 10:12:23 -0800

    Update variableDemo.go

--- variableDemo.go
+++ variableDemo.go
@@ -13,5 +13,13 @@ func main() {
 	c := 30.30
 	fmt.Println(b)
 	fmt.Println(c)
+	
+	//constant
+	const v int = 60
+	fmt.Println(v)
+	
+	// string variable
+	var name string = "XYZ"
+	fmt.Println(name)
 
 }
