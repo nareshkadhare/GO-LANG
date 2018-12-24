5d6f6f8234833498d9e927b43fa50ffc8e8f61bd
nareshkadhare <35559390+nareshkadhare@users.noreply.github.com>
2018-12-24 09:57:16 -0800

    Variable Demo

---
+++ variableDemo.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// used 'var' key word to declare variables
+	var a int
+	a = 10
+	fmt.Println(a)
+
+	//Dynamic Type Declaration
+	b := 20
+	c := 30.30
+	fmt.Println(b)
+	fmt.Println(c)
+
+}
