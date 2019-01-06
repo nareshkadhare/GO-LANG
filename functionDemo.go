aa22fb67c9f2f45a2c5713ab7682703f887f29c8
nareshkadhare <35559390+nareshkadhare@users.noreply.github.com>
2019-01-06 06:28:37 -0800

    function with multiple value return

--- functionDemo.go
+++ functionDemo.go
@@ -10,10 +10,21 @@ func main() {
 	//function calling
 	var sum = sum(a, b)
 	fmt.Printf("SUM OF %d +  %d = %d ", a, b, sum)
+
+	fmt.Printf("\n Before Swaping  A : %d , B : %d ", a, b)
+	
+	//function call handle multiple values
+	a, b = swap(a, b)
+	fmt.Printf("\n After Swaping  A : %d , B : %d ", a, b)
 }
 
 //function defination
 //func function_name( [parameter list] ) [return_types]
 func sum(a int, b int) int {
 	return a + b
+}
+
+//function can return multiple values
+func swap(a int, b int) (int, int) {
+	return b, a
 }
